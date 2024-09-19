b73b66d8c7d36eb1ed5bccc5d0b191c4f213d0be
joao_pedro <joaopedrovieiradossantos326@gmail.com>
2024-09-19 17:46:42 -0300

    Editando as listagens

---
+++ app/Http/Controllers/SiteController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+use \App\Models\Produto;
+
+use Illuminate\Http\Request;
+
+class SiteController extends Controller
+{
+    public function index()
+    {
+       // return "index";
+
+       $produtos = Produto::paginate(3);
+       return view("site.home", compact('produtos'));
+
+    
+    
+
+    }
+    
+    public function details($slug){
+
+        $produto = Produto::where('slug', $slug)->first();
+        
+
+        return view('site.details', compact('produto'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\ProdutoController;
+use App\Http\Controllers\SiteController;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('produtos', ProdutoController::class);
+Route::get('/', [SiteController::class, 'index'])->name('site.index');
+Route::get('/produto/{slug}', [SiteController::class, 'details'])->name('site.details');
